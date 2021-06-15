9ff1106aa91e6aae07012eaf7206d8c2cf115a54
cbilgili <cbilgili@gmail.com>
2021-06-15 22:16:39 +0300

    Adds custom svg icons for side bar

--- src/components/NodeType.js
+++ src/components/NodeType.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {DragSource} from 'react-dnd';
-import {editType} from '../redux/actions/typeEditorActions';
+import { connect } from 'react-redux';
+import { DragSource } from 'react-dnd';
+import { editType } from '../redux/actions/typeEditorActions';
 
 class NodeType extends React.Component {
     render() {
         return this.props.connectDragSource(
-            <div className="btn btn-primary btn-lg btn-block"  onClick={this.onClick.bind(this)}>
+            <div className="btn-block" onClick={this.onClick.bind(this)}>
+                <svg width="121" height="52" viewBox="0 0 121 52" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <rect x="0.5" y="0.5" width="120" height="51" rx="14.5" fill="#A0E2A3" stroke="black" />
+                </svg>
                 {this.props.nodeType.type}
             </div>
         )
